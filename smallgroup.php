692461b5e5e76568b19615ff8bf84cf8e2db4a1f
nikhilt <thiruvengadam.nikhil@gmail.com>
2013-11-27 17:28:31 -0500

    Other variables

--- smallgroup.php
+++ smallgroup.php
@@ -10,9 +10,13 @@
  * 2. category
  * 3. childcare (on / off)
  * 4. weekday (0=Sunday, 1=Monday etc etc)
+ * 5. start_time
  *
- *
- *
+ * What is to be seen by the user
+ * 1. Day of the week (dropdown) (any day)
+ * 2. Postal Code (text)
+ * 3. Children are welcome (checkbox) 
+ * 4. 
  **/
 
 
