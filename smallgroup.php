b50b89c136e8252b3df38a650fd923fc8688a71c
Paul <paul@calvarycambridge.com>
2013-11-27 17:10:19 -0500

    Stuff added

--- smallgroup.php
+++ smallgroup.php
@@ -4,11 +4,19 @@
  * Date: 11/27/2013
  * Time: 4:35 PM
  * Purpose: To create Small group search URL
+ * caytoon.fellowshipone.com is the base URL
+ * Variables -
+ * 1. zipcode (postalcode)
+ * 2. category
+ * 3. childcare (on / off)
+ * 4. weekday (0=Sunday, 1=Monday etc etc)
+ *
+ *
  *
  **/
 
 
 
-echo "Hello World";
+
 
 ?>
